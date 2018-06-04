052d02cebf7f1ac8f2cc7757f397b2db5d1426c3
superyang713 <yangdai713@aim.com>
2018-06-03 20:19:37 -0400

    Learned variables, user input, and echo command

---
+++ hello.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a comment!
+# To run this script, the text file needs to be executable. chomd 755 hello.sh
+
+echo Hello World     # This is a comment, too!
+echo Hello   World
+echo "Hello   World"
+echo Hello * World
+
---
+++ var.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+MY_MESSAGE="Hello World"
+echo $MY_MESSAGE
